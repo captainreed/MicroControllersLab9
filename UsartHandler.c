1496d22ab505ff7acb2ee5bd6be7ecd1ec3865e4
BenjaminRSmith <44481305+BenjaminRSmith@users.noreply.github.com>
2018-11-08 14:27:50 -0700

    set up usart init

--- UsartHandler.c
+++ UsartHandler.c
@@ -33,16 +33,30 @@ void UsartInit()
 /*******************  Bit definition for USART_TDR register  ******************/
 #define  USART_TDR_TDR                       ((uint16_t)0x01FFU)                /*!< TDR[8:0] bits (Transmit Data value) */
 
-	
-	
+USART2->CR1 &= ~USART_CR1_UE; // dissable usart
+USART2->CR1 &= ~USART_CR1_M; // set to be 8 bit message
+USART2->CR2 &= ~USART_CR2_STOP; //set to be 1 stop bit
+USART2->CR1 &= ~USART_CR1_PCE; //set parity
+USART2->CR1 &= ~USART_CR1_OVER8; // set oversampling
+USART2->BRR =  0x208D; // set baud rate
+USART2->CR1 |= (USART_CR1_TE | USART_CR1_RE);//enable transmission and reception
+USART2->CR1 |= USART_CR1_UE;?/enable usart
+while ((USART2->ISR & USART_ISR_TEACK) == 0);
+	{
+		
+	}
+while ((USART2->ISR & USART_ISR_REACK) == 0);
+	{
+		
+	}
 }
 
-void UsartWrite(char* letter)
+void UsartWrite(unit8_t *letter)
 {
 	
 }
 
-void UsartRead()
+void UsartRead(unit8_t *thebuffer)
 {
 	
 	
